cb878259f37d615b8e8bf67722c9949450a25751
GuiDev45 <bortolettodev@gmail.com>
2026-01-10 20:24:43 -0300

    Aula 17 - Concatenando valores

---
+++ Cursos/udemy-001/02-Fundamentos/4-concatena.py
@@ -0,0 +1,21 @@
+name = "Mario"
+year_launch = 2024
+note_movie = 9.5
+
+print("Dados do Filme")
+print("========================")
+
+# Alternativa 1
+print("Nome do filme:", name)
+print("Ano de lançamentos:", year_launch)
+print("Nota do filme", note_movie)
+
+# Alternativa 2
+print("Nome do Filme:", name, "\nAno de Lançamento:", year_launch, "\nNota do Filme:", note_movie)
+
+# Alternativa 3
+print(
+    f"Nome do Filme: {name}\n"
+    f"Ano de Lançamento: {year_launch}\n"
+    f"Nota do Filme: {note_movie}"
+    )
