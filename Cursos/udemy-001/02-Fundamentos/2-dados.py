2c495c58e5e210d2e7d3fd2b3876be47fe4b953c
GuiDev45 <bortolettodev@gmail.com>
2026-01-10 19:36:32 -0300

    Aula 16 - Tipos de Dados

---
+++ Cursos/udemy-001/02-Fundamentos/2-dados.py
@@ -0,0 +1,19 @@
+# PythonFlix
+
+# Variáveis, função e método por convenção, é snake_case e quando é classe é camelCase.
+name = "Senhor dos Anéis"
+year_launch = 2001
+note_movie = 9.5
+plan_in_include = False
+
+# Saída de dados normal
+print(name)
+print(year_launch)
+print(note_movie)
+print(plan_in_include)
+
+# Saída para saber o tipo de variável
+print(type(name))
+print(type(year_launch))
+print(type(note_movie))
+print(type(plan_in_include))
