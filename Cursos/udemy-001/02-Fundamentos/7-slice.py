37b49d01404856aa273ffd2a36e34bde295f3f51
GuiDev45 <bortolettodev@gmail.com>
2026-01-17 17:47:04 -0300

    Aula 20 - Operações e métodos em string

---
+++ Cursos/udemy-001/02-Fundamentos/7-slice.py
@@ -0,0 +1,20 @@
+movieName = "Top Gun"
+# string[início:fim] - índice começa na posição 0
+
+# 1 - Buscar toda string a partir da primeira posição
+print(movieName[0:])
+
+# 2 - Buscar toda string até a última posição
+print(movieName[:7])
+
+# 3 - Buscar toda string da terceira até a última posição
+print(movieName[2:])
+
+# 4 - Buscar toda a string de 2 em 2 caracteres
+print(movieName[::2])
+
+# 5 - Buscar toda a string nos índices impares
+print(movieName[1::2])
+
+# 6 - Inverter uma string
+print(movieName[::-1])
---
+++ Cursos/udemy-001/02-Fundamentos/8-metodosString.py
@@ -0,0 +1,16 @@
+movieName = "Top Gun"
+
+movieDescription = """
+    Top Gun Maverick, é um filme de aviação e aventura
+Muito consagrado na indústria
+"""
+
+print(movieName.upper())
+print(movieName.lower())
+print(movieName.capitalize())
+print(movieName.title())
+print(movieName.center(10, '-')) # Retorna string centralizada com caractere de preenchimento
+print(movieName.find("u"))
+print(movieName.find("o"))
+print(movieName.replace("Top", "Matrix"))
+print(movieName.split(','))
