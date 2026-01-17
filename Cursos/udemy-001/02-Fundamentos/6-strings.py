c997fed0407b44f138b36d3f4f866cdc66761665
GuiDev45 <bortolettodev@gmail.com>
2026-01-17 17:25:59 -0300

    Aula 19 - Utilização de strings

---
+++ Cursos/udemy-001/02-Fundamentos/6-strings.py
@@ -0,0 +1,18 @@
+movieName = "Top Gun"
+movieName2 = "top Gun"
+print(movieName == movieName2) # Case sensitive
+
+movieDescription = """
+    Top Gun Maverick é um filme de aviação e aventura
+Muito consagrado na indústria
+"""
+print(movieDescription)
+
+# 1 - Multiplicação de String
+line = "="
+print(line * 50)
+print(movieDescription)
+
+# 2 - Procurar uma palavra dentro de um texto
+print("Top" in movieName)
+print("ação" in movieName)
