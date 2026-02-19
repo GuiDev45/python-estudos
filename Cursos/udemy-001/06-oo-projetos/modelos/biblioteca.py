9ea5a338e762a0d04dafc6033a1219ce32ce0af7
GuiDev45 <bortolettodev@gmail.com>
2026-02-19 02:51:29 -0300

    Aula 59 - Utilizando property

--- Cursos/udemy-001/06-oo-projetos/modelos/biblioteca.py
+++ Cursos/udemy-001/06-oo-projetos/modelos/biblioteca.py
@@ -2,7 +2,7 @@ class Biblioteca:
     bibliotecas = []
     def __init__(self, nome):
         self.nome = nome
-        self.ativo = False
+        self._ativo = False
         Biblioteca.bibliotecas.append(self)
 
     def __str__(self):
@@ -12,10 +12,17 @@ class Biblioteca:
         for biblioteca in Biblioteca.bibliotecas:
             print(f"{biblioteca.nome} | {biblioteca.ativo}")
 
+    def alterna_estado(self):
+        self._ativo = not self._ativo
+
+    @property
+    def ativo(self):
+        return "ativada" if self._ativo else "desativada"
+
+
 biblioteca_cidade = Biblioteca("Biblioteca da Cidade")
 biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
 
-print(biblioteca_cidade)
-print(biblioteca_shopping)
+biblioteca_shopping.alterna_estado() # Ativar apenas shooping
 
 Biblioteca.listar_bibliotecas()
