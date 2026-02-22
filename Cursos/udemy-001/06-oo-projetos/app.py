3325b68ba0c0cc6c121a53ca3023c993e9879086
GuiDev45 <bortolettodev@gmail.com>
2026-02-22 16:33:18 -0300

    Aula 65 - Relacionando Dados na Classe Herdada

--- Cursos/udemy-001/06-oo-projetos/app.py
+++ Cursos/udemy-001/06-oo-projetos/app.py
@@ -6,7 +6,13 @@ biblioteca_cidade = Biblioteca("Biblioteca da Cidade")
 biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
 
 livro1 = Livro("1984", "George Orwell", 30.0, "084-3254")
+livro2 = Livro("Brave New World", "Aldous Huxley", 25.0, "022-1978")
+
 revista1 = Revista("National Geographic", "John Doe", 15.0, "Quinta")
+
+biblioteca_cidade.adicionar_item(livro1)
+biblioteca_cidade.adicionar_item(revista1)
+biblioteca_cidade.adicionar_item(livro2)
 
 # biblioteca_cidade.alterna_estado()
 # biblioteca_cidade.receber_avaliacao("Fulano", 8.5)
@@ -16,6 +22,7 @@ def main():
    # Biblioteca.listar_bibliotecas()
    print(vars(livro1))
    print(vars(revista1))
+   biblioteca_cidade.exibir_itens()
 
 if __name__ == "__main__":
     main()
--- Cursos/udemy-001/06-oo-projetos/modelos/biblioteca.py
+++ Cursos/udemy-001/06-oo-projetos/modelos/biblioteca.py
@@ -1,10 +1,12 @@
 from modelos.avaliacao import Avaliacao
+from modelos.itens.item_biblioteca import ItemBiblioteca
 class Biblioteca:
     bibliotecas = []
     def __init__(self, nome):
         self.nome = nome
         self._ativo = False
         self._avaliacao = []
+        self._itens = []
         Biblioteca.bibliotecas.append(self)
 
     def __str__(self):
@@ -34,3 +36,19 @@ class Biblioteca:
         soma = sum(avaliacao._nota for avaliacao in self._avaliacao)
         media = round (soma / len(self._avaliacao))
         return media
+    
+    def adicionar_item(self, item):
+        if isinstance(item, ItemBiblioteca):
+            self._itens.append(item)
+
+    def exibir_itens(self):
+        print(f"Itens da Biblioteca {self.nome}\n")
+        for  i, item in enumerate(self._itens, start=1):
+            if hasattr(item, "isbn"):
+                msg_livro = f"{i}. (Livro) -> Título: {item._titulo} | Autor: {item._autor} | Preço: {item._preco} | ISBN: {item.isbn}"
+                print(msg_livro)
+            else:
+                msg_revista = f"{i}. (Revista) -> Título: {item._titulo} | Autor: {item._autor} | Preço: {item._preco} | Edição: {item.edicao}"
+                print(msg_revista)
+
+
