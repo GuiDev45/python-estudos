fd6e3d7b8111d3eb994e8c9ada2cfb3d8c20f70a
GuiDev45 <bortolettodev@gmail.com>
2026-02-22 16:12:54 -0300

    Aula 64 - Utilizando Herença

--- Cursos/udemy-001/06-oo-projetos/app.py
+++ Cursos/udemy-001/06-oo-projetos/app.py
@@ -1,15 +1,21 @@
 from modelos.biblioteca import Biblioteca
+from modelos.itens.livro import Livro
+from modelos.itens.revista import Revista
 
 biblioteca_cidade = Biblioteca("Biblioteca da Cidade")
 biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
 
-biblioteca_cidade.alterna_estado()
+livro1 = Livro("1984", "George Orwell", 30.0, "084-3254")
+revista1 = Revista("National Geographic", "John Doe", 15.0, "Quinta")
 
-biblioteca_cidade.receber_avaliacao("Fulano", 8.5)
-biblioteca_cidade.receber_avaliacao("Sicrano", 9.5)
+# biblioteca_cidade.alterna_estado()
+# biblioteca_cidade.receber_avaliacao("Fulano", 8.5)
+# biblioteca_cidade.receber_avaliacao("Sicrano", 9.5)
 
 def main():
-    Biblioteca.listar_bibliotecas()
+   # Biblioteca.listar_bibliotecas()
+   print(vars(livro1))
+   print(vars(revista1))
 
 if __name__ == "__main__":
     main()
