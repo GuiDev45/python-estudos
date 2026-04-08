d90f422cec1b84b73c87b58e79e3d17c038edfec
GuiDev45 <bortolettodev@gmail.com>
2026-04-08 03:23:04 -0300

    Aula 148 - Utilizando Path Parameter

--- Cursos/udemy-001/14-fastapi/01-fundamentos/api.py
+++ Cursos/udemy-001/14-fastapi/01-fundamentos/api.py
@@ -2,8 +2,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+jogadores = {
+    1: {
+        "nome": "Rony",
+        "idade":28,
+        "time": "Palmeiras"
+    },
+    2:{
+        "nome":"Gustavo Gomez",
+        "idade": 29,
+        "time": "Palmeiras"
+    }
+}
+
 @app.get("/")
 def inicio():
-    return {"Mensagem": "Olá mundo"}
+    return jogadores
 
-#uvicorn 5-fast:app --reload
+@app.get("/get-jogador/{id_jogador}")
+def get_jogador(id_jogador:int):
+    return jogadores[id_jogador]
+
+#get-jogador/1 - Path Parameter
+
+#get-jogador/?id=1 - Query Parameter
+
