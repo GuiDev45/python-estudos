2d00e8beba7e07d30d50e6bab41ec7a78cde308a
GuiDev45 <bortolettodev@gmail.com>
2026-04-08 03:57:16 -0300

    Aula 149 - Atu 02

--- Cursos/udemy-001/14-fastapi/01-fundamentos/api.py
+++ Cursos/udemy-001/14-fastapi/01-fundamentos/api.py
@@ -3,35 +3,30 @@ from fastapi import FastAPI
 app = FastAPI()
 
 jogadores = {
-    1: {
-        "nome": "Rony",
-        "idade":28,
-        "time": "Vasco"
-    },
-    2:{
-        "nome":"Gustavo Gomez",
-        "idade": 29,
-        "time": "Palmeiras"
-    }
+    1: {"nome": "Rony", "idade": 28, "time": "Vasco"},
+    2: {"nome": "Gustavo Gomez", "idade": 29, "time": "Palmeiras"},
 }
+
 
 @app.get("/")
 def inicio():
     return jogadores
 
+
 @app.get("/get-jogador/{id_jogador}")
-def get_jogador(id_jogador:int):
+def get_jogador(id_jogador: int):
     return jogadores[id_jogador]
 
+
 @app.get("/get-jogador-time")
-def get_jogador_time(time:str):
+def get_jogador_time(time: str):
     for jogador_id in jogadores:
         if jogadores[jogador_id]["time"] == time:
             return jogadores[jogador_id]
-    return {"Dados": "Não foi encontrado"}
-    
-#get-jogador/1 - Path Parameter
+    return {"Dados": "NÃ£o foi encontrado"}
 
-#get-jogador/?id=1 - Query Parameter
-#get-jogador-time?time="fsfd"
 
+# get-jogador/1 - Path Parameter
+
+# get-jogador/?id=1 - Query Parameter
+# get-jogador-time?time="fsfd"
