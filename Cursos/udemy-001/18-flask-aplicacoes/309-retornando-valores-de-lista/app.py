c4277ff1626ea030200694c2d6a00b04a08e1ab6
GuiDev45 <bortolettodev@gmail.com>
2026-04-16 00:24:38 -0300

    Aula 309 - valores de lista

---
+++ Cursos/udemy-001/18-flask-aplicacoes/309-retornando-valores-de-lista/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def principal():
+	frutas = ["Morango", "Uva", "Laranja", "Mamão", "Maçã", "Pêra", "Melão", "Abacaxi"]
+	return render_template("index.html",frutas=frutas)
+
+@app.route('/sobre')
+def sobre():
+	return render_template("sobre.html")
